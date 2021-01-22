224e468a454fbf118dbce4b8afc672ee7d62020f
Andrii Baldyniuk <baldynuk.andrey87@gmail.com>
2021-01-22 17:24:44 +0200

    clear branch for start

--- angularBase/src/app/app.module.ts
+++ angularBase/src/app/app.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { UsersComponent } from './users/users.component';
-import { UserComponent } from './user/user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent,
-    UsersComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
