b3473410b09e88b5b05be750961041fd293ce214
Andrii Baldyniuk <baldynuk.andrey87@gmail.com>
2021-01-23 17:17:06 +0200

    homa task 2 Done

--- angularBase/src/app/app.module.ts
+++ angularBase/src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { UsersComponent } from './components/users/users.component';
-import { UserComponent } from './components/user/user.component'
+import { UserComponent } from './components/user/user.component';
+import { PostComponent } from './components/post/post.component';
+import { PostsComponent } from './components/posts/posts.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    PostComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
